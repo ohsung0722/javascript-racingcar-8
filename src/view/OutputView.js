e27cfb103dada3063f2f736edc29734abe318bd4
ohsung0722 <99doldol@naver.com>
2025-10-22 23:55:37 +0900

    feat(io): Console.print() 기반 OutputView 클래스 구현
    게임 실행 과정, 결과, 우승자 출력, 각 종 에러 메시지를 출력하는 OutputView 클래스 구현

---
+++ src/view/OutputView.js
@@ -0,0 +1,25 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class OutputView {
+  printError(errorMessage) {
+    Console.print(`[ERROR] ${errorMessage}`);
+  }
+
+  printWinner(winners) {
+    Console.print(`최종 우승자 : ${winners.join(",")}`);
+  }
+
+  printRound(cars) {
+    cars.forEach((car) => {
+      Console.print(`${car.name} : ${"-".repeat(car.position)}`);
+    });
+
+    this.#printLineBreak;
+  }
+
+  #printLineBreak() {
+    Console.print("");
+  }
+}
+
+export default OutputView;
