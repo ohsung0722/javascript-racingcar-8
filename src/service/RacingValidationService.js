8516fca668deba83493e183034cb9e9c56bf1923
ohsung0722 <99doldol@naver.com>
2025-10-23 19:50:41 +0900

    feat(service): 도메인 로직에 필요한 검증 serviceLayer 구현

---
+++ src/service/RacingValidationService.js
@@ -0,0 +1,25 @@
+import {
+  validateInteger,
+  validateMaxLength,
+  validateNoDuplicates,
+  validateNotEmpty,
+  validatePositiveNumber,
+} from "../utils/Validator";
+
+class RacingValidationService {
+  validateCarNames(names) {
+    validateNoDuplicates(names, "자동차 이름은 중복될 수 없습니다.");
+
+    names.forEach((name) => {
+      validateNotEmpty(name, "자동차 이름은 비어있을 수 없습니다.");
+      validateMaxLength(name, 5, "자동차 이름");
+    });
+  }
+
+  validateTryCount(count) {
+    validatePositiveNumber(count, "시도 횟수는 1 이상의 숫자여야 합니다.");
+    validateInteger(count, "시도 횟수는 정수여야 합니다.");
+  }
+}
+
+export default RacingValidationService;
