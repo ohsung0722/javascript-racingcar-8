3bffc9837b7e0409a36ccc9209d8dfc36c4506b3
ohsung0722 <99doldol@naver.com>
2025-10-23 17:29:59 +0900

    feat(io): 사용자 입력 함수에 입력값 파싱 기능 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,4 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
+import { parseNumber, splitByComma } from "../utils/Parser.js";
 
 class InputView {
   async inputCars() {
@@ -6,13 +7,13 @@ class InputView {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
 
-    return input;
+    return splitByComma(input);
   }
 
   async inputTry() {
     const input = await Console.readFileAsync("시도할 횟수는 몇 회인가요?");
 
-    return input;
+    return parseNumber(input);
   }
 }
 
