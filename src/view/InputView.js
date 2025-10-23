637eb3642234974d9e99a3ef8f36dfaaa541ae9f
ohsung0722 <99doldol@naver.com>
2025-10-23 19:43:22 +0900

    fix(io): 사용자 입력 시 공백 검증할 수 있도록 기능 수정

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,5 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 import { parseNumber, splitByComma } from "../utils/Parser.js";
+import { validateNotEmpty } from "../utils/Validator.js";
 
 class InputView {
   async inputCars() {
@@ -7,11 +8,15 @@ class InputView {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
 
+    validateNotEmpty(input, "자동차 이름을 1개 이상 입력해야 합니다.");
+
     return splitByComma(input);
   }
 
   async inputTry() {
     const input = await Console.readFileAsync("시도할 횟수는 몇 회인가요?");
+
+    validateNotEmpty(input, "시도 횟수를 입력해야 합니다.");
 
     return parseNumber(input);
   }
